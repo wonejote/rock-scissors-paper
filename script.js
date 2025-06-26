22586f8457f4575292eeb912fa2bbe137f792a24
Antonio <antonio.pulgarin97@gmail.com>
2025-06-26 11:48:16 -0500

    funciones de ganar y contar actualizadas, el juego ya funciona correctamente

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 
 
-let opciones = ["piedra","papel","tijera"];
+const opciones = ["Piedra","Papel","Tijeras"];
 
 function elegir()
-{
-    
+{   
     let x = Math.floor( Math.random()*3  );
     return opciones[x];
-
 }
 
 
@@ -16,51 +14,49 @@ let contadorJugador = 0;
 let contadorPC = 0;
 
 const mensaje = document.querySelector(".victorias");
-mensaje.textContent = "Victorias : " + contadorJugador
+mensaje.textContent = "Victorias : " + contadorJugador;
 
 const mensaje2 = document.querySelector(".derrotas");
 mensaje2.textContent = "Derrotas : " + contadorPC;
 
 const mensaje3 = document.querySelector(".usado");
-mensaje3.textContent = "aun no";
+
+
+const mensaje4 = document.querySelector(".computador");
 
 function usar(numero){
-    if (numero === 1)
-    {
-        mensaje3.textContent = "1";
-        contadorJugador ++;
-    }
-    if (numero === 2)
-    {
-        mensaje3.textContent = "2";
-    }
-    if (numero === 3)
-    {
-        mensaje3.textContent = "3";
-    }
-    
+    if (numero === 1) {mensaje3.textContent = "Piedra";}
+    if (numero === 2) {mensaje3.textContent = "Tijeras";}
+    if (numero === 3) {mensaje3.textContent = "Papel";}  
+    let compu = elegir();
+    mensaje4.textContent = compu; 
+    ganador(numero,compu);
+}
+function ganador(numero,compu)
+{
+    if (numero === 1 && compu === "Tijeras"){contadorJugador++;}
+    if (numero === 1 && compu === "Papel"){contadorPC++;}
+
+    if (numero === 2 && compu === "Papel"){contadorJugador++;}
+    if (numero === 2 && compu === "Piedra"){contadorPC++;}
+
+    if (numero === 3 && compu === "Piedra"){contadorJugador++;}
+    if (numero === 3 && compu === "Tijeras"){contadorPC++;}
+
+    mensaje.textContent = "Victorias : " + contadorJugador;
+    mensaje2.textContent = "Derrotas : " + contadorPC;
+
 }
 
-const boton1 = document.getElementById("uno");
-boton1.addEventListener("click",function()
-{usar(1);
-    const mensaje = document.querySelector(".victorias");
-mensaje.textContent = "Victorias : " + contadorJugador
 
-}
+const botones = document.querySelectorAll(".botond");
 
-);
-const boton2 = document.getElementById("dos");
-boton2.addEventListener("click",function()
-{usar(2);}
-
-);
-const boton3 = document.getElementById("tres");
-boton3.addEventListener("click",function()
-{usar(3);}
-
-); 
-
-
+botones.forEach(boton => {
+  boton.addEventListener("click", function () {
+    if (boton.id === "uno") {usar(1);}
+     else if (boton.id === "dos") {usar(2);}
+      else if (boton.id === "tres") {usar(3);}
+  });
+});
 
 
